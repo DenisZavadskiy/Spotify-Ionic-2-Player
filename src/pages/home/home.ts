8b1129f4e08386db00b90d1f90484c47236cb16e
Unknown <denis.v.zavadskiy@gmail.com>
2017-02-18 22:29:35 +0200

    update(home page): add functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavController, LoadingController} from 'ionic-angular';
 
-import { NavController } from 'ionic-angular';
+import {SpotifyService} from "../../providers/spotify.service";
+import {TrackPage} from "../track/track";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tracks: any[];
+  loader: any;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+              private spotify: SpotifyService,
+              private loadingCtrl: LoadingController) {
   }
 
+  search(query) {
+    this.loader = this.loadingCtrl.create();
+    this.loader.present();
+
+    this.spotify.searchTracks(query, 5)
+      .subscribe(
+        (res: any) => {
+          this.tracks = res.tracks.items;
+          this.loader.dismiss();
+          console.log(this.tracks);
+        },
+        (err) => {
+          this.loader.dismiss();
+          alert(err);
+        });
+  }
+
+  showMore(track) {
+    this.navCtrl.push(TrackPage, {
+      track: track
+    });
+  }
+
+  ngOnInit() {
+  }
 }
