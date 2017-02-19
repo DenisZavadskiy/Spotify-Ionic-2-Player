337b57f94716ad94e1d980c9484e66a0f8b6e3ed
Unknown <denis.v.zavadskiy@gmail.com>
2017-02-19 22:35:10 +0200

    update(settings page)
    add limit choosing range

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,22 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Settings page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
+  limit: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+    this.limit = localStorage.getItem('limit');
   }
 
+  ionViewWillLeave() {
+    localStorage.setItem('limit', this.limit);
+  }
 }
