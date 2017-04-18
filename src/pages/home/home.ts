3b2ae54ad7051d6c788db3ff2a1a66a9ff0a05ae
Unknown <denis.v.zavadskiy@gmail.com>
2017-04-18 22:40:31 +0300

    update(home page): add toast

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {NavController, LoadingController} from 'ionic-angular';
+import {NavController, LoadingController, ToastController} from 'ionic-angular';
+import {Storage} from "@ionic/storage";
 
 import {SpotifyService} from "../../providers/spotify.service";
 import {TrackPage} from "../track/track";
@@ -9,13 +10,16 @@ import {TrackPage} from "../track/track";
   templateUrl: 'home.html'
 })
 export class HomePage {
-  tracks: any[];
+  tracks: any[] = [];
   loader: any;
   limit: string;
+  playlist: any[] = [];
 
   constructor(public navCtrl: NavController,
               private spotify: SpotifyService,
-              private loadingCtrl: LoadingController) {
+              private loadingCtrl: LoadingController,
+              private storage: Storage,
+              public toastCtrl: ToastController) {
   }
 
   search(query) {
@@ -36,9 +40,42 @@ export class HomePage {
         });
   }
 
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 1500,
+      position: 'bottom'
+    });
+
+    toast.present();
+  }
+
+  ionViewWillEnter() {
+    this.storage.get('playlist')
+      .then(val => {
+        if(val) {
+          this.playlist = val;
+        } else {
+          this.storage.set('playlist', this.playlist);
+        }
+      });
+  }
+
   showMore(track) {
     this.navCtrl.push(TrackPage, {
       track: track
     });
+  }
+
+  addToPlaylist(track) {
+    let foundTrack: any = this.playlist.find(item => item.id === track.id);
+
+    if (foundTrack) {
+      this.presentToast(`Track '${foundTrack.name}' is present in playlist`);
+    } else {
+      this.playlist.push(track);
+      this.storage.set('playlist', this.playlist);
+      this.presentToast(`Track '${track.name}' was added successfully`);
+    }
   }
 }
